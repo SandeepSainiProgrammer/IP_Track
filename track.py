a41412670e77178ceb1b43b1e1707a34a69c82e8
RohitSaxsena <Acc3ssDenied001@gmail.com>
2014-06-28 10:39:21 +0530

    track.py

---
+++ track.py
@@ -0,0 +1,22 @@
+import pygeoip
+
+print '\t\t\t ######################################'
+print '\t\t\t ##    Proudly Made By An INDIAN     ##'
+print '\t\t\t ##   Author : Rohit Saxsena INDIA   ##'
+print '\t\t\t ######################################'
+
+gi = pygeoip.GeoIP('/opt/GeoIP/Geo.dat')
+def TargetIP(target):
+	rec = gi.record_by_name(target)
+	city = rec['city']
+	region = rec['region_name']
+	country = rec['country_name']
+	long = rec['longitude']
+	lat = rec['latitude']
+	print '[*] Target: ' + target + ' Geo-located. '
+	print '[+] '+str(city)+', '+str(region)+', '+str(country)
+	print '[+] Latitude: '+str(lat)+ ', Longitude: '+ str(long)
+
+host = raw_input('Enter Target :')
+target = host
+TargetIP(target)
